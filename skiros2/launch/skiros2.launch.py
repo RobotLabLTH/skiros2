6fb47486b3ce59e159330df292b7d90e0c51e371
Matthias Mayr <matthias.mayr@cs.lth.se>
2024-02-19 15:52:14 +0000

    New: Updates skiros2.launch.py
    - Accepts arguments
    - Loads reasoner from std_lib
    - Starts skill manager

--- skiros2/launch/skiros2.launch.py
+++ skiros2/launch/skiros2.launch.py
@@ -1,10 +1,10 @@
 import os
 import yaml
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
 
 
 def load_file(package_name, file_path):
@@ -30,16 +30,39 @@ def load_yaml(package_name, file_path):
 
 
 def generate_launch_description():
-    # robot_description_config = load_file('ur_description', 'urdf/ur5.urdf')
-    # robot_description = {'robot_description' : robot_description_config}
+    libraries_list = LaunchConfiguration('libraries_list')
+    skill_list = LaunchConfiguration('skill_list')
+    robot_name = LaunchConfiguration('robot_name')
+
+    libraries_list_arg = DeclareLaunchArgument(
+        'libraries_list',
+        default_value="['']"
+    )
+    skill_list_arg = DeclareLaunchArgument(
+        'skill_list',
+        default_value="['']"
+    )
+    robot_name_arg = DeclareLaunchArgument(
+        'robot_name',
+        default_value='robot'
+    )
 
     # skiros_config_file = get_package_share_directory('skiros2') + "/cfg/skiros_config.yaml"
     wm_config = {
         "workspace_dir": get_package_share_directory('skiros2') + "/owl",
         "init_scene": "",
         "verbose": False,
-        # "reasoners_pkgs": ["skiros2_std_reasoners"],
+        "reasoners_pkgs": ["skiros2_std_reasoners"],
         # "load_contexts": [],
+    }
+
+    skill_mgr_config = {
+        "prefix": "skiros",
+        "verbose": False,
+        "libraries_list": libraries_list,
+        "skill_list": skill_list,
+        "robot_name": robot_name,
+        "deploy": False
     }
 
     static_tf = Node(package='tf2_ros',
@@ -57,5 +80,17 @@ def generate_launch_description():
                executable='rqt_gui',
                # arguments=["-s skiros2_gui.gui.Skiros"], # does not find the plugin. Not sure why - it appears in the list and can be selected with "ros2 run"
                output='screen')
+    
+    skill_mgr = Node(package='skiros2_skill',
+                     executable='skill_manager_node',
+                     output='screen',
+                     respawn=True,
+                     parameters=[skill_mgr_config])
 
-    return LaunchDescription([static_tf, wm, gui])
+    return LaunchDescription([libraries_list_arg,
+                              skill_list_arg,
+                              robot_name_arg,
+                              static_tf,
+                              wm,
+                              gui,
+                              skill_mgr])
