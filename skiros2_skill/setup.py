a12f9c3da90ca18691835d3ad540fffa2cd8c57d
Matthias Mayr <matthias.mayr@cs.lth.se>
2024-02-20 20:35:02 +0000

    New: Declares skiros2_skill.nodes subpackage

--- skiros2_skill/setup.py
+++ skiros2_skill/setup.py
@@ -5,7 +5,7 @@ package_name = 'skiros2_skill'
 setup(
     name=package_name,
     version='1.0.5',
-    packages=[package_name, package_name + "/core", package_name + "/ros"],
+    packages=[package_name, package_name + "/core", package_name + "/nodes", package_name + "/ros"],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
