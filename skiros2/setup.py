6b6cc38df79a99e16ddc2f0b8e9f49fadaa9f284
Matthias Mayr <matthias.mayr@cs.lth.se>
2024-08-17 13:18:45 +0000

    Fix: Explicitly define "packages" in skiros2 setup.py
    Otherwise building fails with "error: Multiple top-level packages discovered in a flat-layout: ['cfg', 'owl', 'res', 'launch', 'resource']."

--- skiros2/setup.py
+++ skiros2/setup.py
@@ -6,6 +6,7 @@ package_name = 'skiros2'
 
 setup(
     name=package_name,
+    packages=[],
     version='1.0.5',
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
