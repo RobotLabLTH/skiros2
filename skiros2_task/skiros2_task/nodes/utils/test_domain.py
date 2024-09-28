3b025a1231ef61bb2abf8874b2ba10a6eeeedc44
Matthias Mayr <matthias.mayr@cs.lth.se>
2024-09-28 11:12:28 +0000

    Chore: Renames python files for task planner to .py

--- skiros2_task/skiros2_task/nodes/utils/test_domain.py
+++ skiros2_task/skiros2_task/nodes/utils/test_domain.py
@@ -1,5 +1,7 @@
-#!/usr/bin/env python
 import skiros2_task.core.pddl_interface as pddl
 
-if __name__ == '__main__':
+def main():
     print(pddl.PddlInterface().invokePlanner(False))
+
+if __name__ == '__main__':
+    main()
